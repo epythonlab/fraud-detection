7edce5844d7615df9b2e9665ca595160f6b3e3ab
Dejene Techane <dtechane6@gmail.com>
2024-10-19 04:43:44 -0400

    task 1: updated in feature engineering

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-class DataPreprocessor:
+class LoadData:
     """
     A class for preprocessing a dataset, including loading, cleaning, and handling missing values.
 
